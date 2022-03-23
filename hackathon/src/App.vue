e032098434c07a32bca1ad4c0e156d6d92fe91e6
jasonyeske <jasonyeske@sasktel.net>
2022-03-23 13:15:19 -0600

    added nav bar and footer

--- hackathon/src/App.vue
+++ hackathon/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheNavBar />
+  <SocialMedia />
+  <TheFooter />
+  
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheNavBar from './components/layout/TheNavBar.vue'
+import TheFooter from './components/layout/TheFooter.vue'
+import SocialMedia from './components/SocialMedia.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheNavBar, TheFooter, SocialMedia
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ hackathon/src/components/layout/TheNavBar.vue
@@ -0,0 +1,67 @@
+<style>
+
+
+nav {
+  
+  background-color: white;
+  font-size: 19px;
+  
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+  margin-right: 2rem;
+  flex-wrap: wrap;
+  flex-grow: 2;
+  justify-content: space-between;
+  margin-top:15px;
+  }
+
+
+li > a:hover {
+  color: green;
+  text-decoration: underline;
+}
+li > a{
+  text-decoration: none;
+  color: black;
+   font-family: 'Roboto', sans-serif;
+   
+}
+
+li > a:visited {
+  text-decoration:black;
+  color: black;
+}
+
+img {
+width:110px;
+height:50px;
+
+}
+</style>
+
+
+
+<template>
+    <div>
+<nav role="navigation">
+  
+  <ul>
+   <li><img src="https://1000logos.net/wp-content/uploads/2018/04/Logo-Stussy.png"></li>
+    <li><a href="#">Shop</a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#">Search</a></li>
+    <li><a href="#">Bag</a></li>
+  </ul>
+</nav>
+
+    </div>
+</template>
